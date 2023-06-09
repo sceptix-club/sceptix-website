1510e4adb1448305f61f781e136afb0b2b85c5ce
charispinto <21h16.charis@sjec.ac.in>
2023-06-10 01:07:44 +0530

    added footer

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
     <section id="about" className="bg-gray-900 py-10">
       <div className="container grid grid-cols-1 xl:grid-cols-2 gap-6 mt-12">
         <div>
-          <h2 className="text-6xl  text-center font-bold mb-14 bg-clip-text text-transparent bg-gradient-to-r  from-green-400 to-sky-400 ">About Us </h2>    {/* from-sky-500 from-1%  to-purple-400 to-70%*/}
+          <h2 className="text-6xl font-mono text-center font-bold mb-14 bg-clip-text text-transparent bg-gradient-to-r  from-green-400 to-sky-400 ">About Us </h2>    {/* from-sky-500 from-1%  to-purple-400 to-70%*/}
           <p className=" text-xl font-mono pl-20 text-white text-center xl:text-left ">
             <p>
               Welcome to The sceptix club, where freedom and innovation converge. We are dedicated to empowering individuals with the liberating force of open-source software. Over the past year, we have conducted engaging sessions, organized Linux installation drives, and hosted captivating coding events and hackathons. Together, we have witnessed the transformative power of collaboration and the limitless possibilities it brings.
