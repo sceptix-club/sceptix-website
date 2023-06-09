b2cc7be53f788c020199de72501dfcb22525180f
charispinto <21h16.charis@sjec.ac.in>
2023-06-09 17:01:25 +0530

    removed sidebar border

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -7,12 +7,7 @@ const AboutUs = () => {
         <div>
           <h2 className="text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-sky-500 from-2% to-purple-400 to-20% ">About Us </h2>
           <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan faucibus ipsum, eget tempor
-            turpis fringilla vitae. Curabitur aliquet tortor at elit convallis, ac volutpat purus euismod.
-          </p>
-          <p>
-            Nunc ut aliquam felis, id lacinia purus. Vestibulum tincidunt nunc nec interdum vulputate. Aliquam erat
-            volutpat. Sed eu lacus vitae velit lacinia pellentesque. Mauris in mauris quis mauris lacinia pretium.
+          Welcome to The sceptix club, where freedom and innovation converge. We are dedicated to empowering individuals with the liberating force of open-source software. Over the past year, we have conducted engaging sessions, organized Linux installation drives, and hosted captivating coding events and hackathons. Together, we have witnessed the transformative power of collaboration and the limitless possibilities it brings. As we forge ahead, our commitment to expanding our community and contributing to larger-scale open-source projects burns brighter than ever. Join us at The sceptix club and let's celebrate the journey of freedom, creativity, and boundless minds
           </p>
         </div>
         <div>
