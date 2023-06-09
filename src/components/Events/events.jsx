3227cb611c7da8c52b6df8e8c080f944d62d31f0
samwin <samwinpereira@gmail.com>
2023-06-09 20:16:26 +0530

    dumped work on charis

--- src/components/Events/events.jsx
+++ src/components/Events/events.jsx
@@ -10,7 +10,7 @@ import { AiFillLinkedin } from 'react-icons/ai';
   const EventPage = () => {
    const eventPast = [
     {
-    name: "hacktofuture",    
+    eventName: "hacktofuture",    
     date: "July 2022",
     image: 'hack.jpg',
     eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
@@ -18,7 +18,7 @@ import { AiFillLinkedin } from 'react-icons/ai';
     }, 
 
     {
-        name: "hacktofuture",    
+        eventName: "hacktofuture",    
         date: "July 2022",
         image: 'hack.jpg',
         eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
@@ -26,7 +26,7 @@ import { AiFillLinkedin } from 'react-icons/ai';
     }, 
 
     {
-        name: "hacktofuture",    
+        eventName: "hacktofuture",    
         date: "July 2022",
         image: 'hack.jpg',
         eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
@@ -35,7 +35,7 @@ import { AiFillLinkedin } from 'react-icons/ai';
 
 
     {
-        name: "hacktofuture",    
+        eventName: "hacktofuture",    
         date: "July 2022",
         image: 'hack.jpg',
         eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
@@ -43,7 +43,7 @@ import { AiFillLinkedin } from 'react-icons/ai';
     }, 
 
     {
-        name: "hacktofuture",    
+        eventName: "hacktofuture",    
         date: "July 2022",
         image: 'hack.jpg',
         eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
@@ -60,18 +60,18 @@ import { AiFillLinkedin } from 'react-icons/ai';
 
       
     <div className='text-center mt-3'>
-    <h1 className="text-4xl font-bold mb-8 m-auto bg-gradient-to-l from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%   bg-clip-text text-transparent">Past Events</h1>  
+    <h1 className="text-6xl font-bold mb-8 m-auto bg-gradient-to-l from-sky-500 from-1%  to-purple-400 to-70%   bg-clip-text text-transparent">Events</h1>  
     </div>    
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 ml-9">
         {eventPast.map((member, index) => (
-          <div key={index} className="bg-white shadow rounded-lg  flex flex-col   border-gray hover:shadow-xl">
+          <div key={index} className="bg-gray-900 shadow   flex flex-col   border-gray hover:shadow-xl">
             <img
               src={member.image}
-              alt={member.name}
-              className="w-full h-65 object-cover "
+              alt={member.eventName}
+              className="w-full  h-40 object-cover "
             />
-            <h2 className="text-3xl font-bold mb-2 p-2 mt-3 ">{member.name}</h2>
-            <p className='bg-gray-200 p-2 rounded-full w-fit size ml-3'>{member.date}</p>
+            <h2 className="text-xl text-white font-bold mb-2 p-2 mt-3 ">{member.eventName}</h2>
+            <p className='bg-gray-200 text-xs p-2 rounded-full w-fit size ml-3'>{member.date}</p>
             <p className="text-gray-500 p-3">{member.eventInfo}</p>
             <div className='flex flex-row justify-center mt-2'>            
             </div>
