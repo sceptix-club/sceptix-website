e2c3d9ee90d6766f5bef82bae552281e5eebb609
joywin2003 <21a20.joywin@sjec.ac.in>
2023-06-10 15:54:25 +0530

    added completed cards

--- src/components/joywin-cards/Card.js
+++ src/components/joywin-cards/Card.js
@@ -2,22 +2,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import './Cards.css';
-import image from './WhatsApp Image 2023-06-09 at 10.57.28 PM.jpeg'; // Fixed the file extension
 
 const Cards = (props) => {
-    const [isExpanded, setIsExpanded] = useState(false);
-    return (
-        <div className="Card-content">
-            <div className='Card-image'>
-                <img src={image} alt="Test Image" />
-            </div>
-            <div className='Card-info'>
-                <h3>Session on Anime</h3>
-                <h5><FontAwesomeIcon icon={faCalendarAlt} /> 12/15/2023</h5>
-            </div>
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleCardClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div className={`Card-content ${isExpanded ? 'expanded' : ''}`} onClick={handleCardClick}>
+      <div className="Card-image">
+        <img src={props.image} alt="Test Image" />
+      </div>
+      <div className="Card-title">
+          <h3>{props.eventName}</h3>
+          <h5>
+            <FontAwesomeIcon icon={faCalendarAlt} /> {props.date}
+          </h5>
+      </div>
+      {isExpanded ? (
+        <div className="Card-info">
+          <h3>Session on Anime</h3>
+          <h5>
+            <FontAwesomeIcon icon={faCalendarAlt} /> 12/15/2023
+          </h5>
+          <p>
+            {props.eventInfo}
+          </p>
         </div>
+      ) : null}
+    </div>
   );
-}
+};
 
 export default Cards;
 
