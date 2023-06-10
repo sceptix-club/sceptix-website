f1008ee7c6aff9cf34ec728582ce4eb6c7349366
joywin2003 <21a20.joywin@sjec.ac.in>
2023-06-10 09:29:44 +0530

    fixed team.jsx

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -1,169 +1,171 @@
 import React from 'react';
-import {AiFillGithub} from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
 import { AiFillLinkedin } from 'react-icons/ai';
 import Sidebar from '../sidenav/sidebar';
 import Footer from '../footer/footer.jsx';
 
 
 
+const MemberCard = ({ member }) => {
+  return (
+    <div className="bg-gray-900 hover:bg-gray-800 border-gray-400 border-solid border-1 transition duration-150 ease-out hover:ease-in shadow p-4 flex flex-col items-center rounded-lg">
+      <img
+        src={member.image}
+        alt={member.name}
+        className="w-48 h-48 object-cover mb-4 rounded-full mt-4"
+      />
+      <h2 className="text-xl text-white font-bold mb-3">{member.name}</h2>
+      <p className="text-gray-500">{member.role}</p>
+      <div className="flex flex-row justify-center mt-2">
+        <div className="m-2">
+          <a href={member.gitlink}>
+            <AiFillGithub color="white" size={30} />
+          </a>
+        </div>
+        <div className="m-2">
+          <a href={member.linked}>
+            <AiFillLinkedin color="white" size={30} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-  const TeamPage = () => {
+const TeamPage = () => {
   const teamMembers = [
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
       name: 'Pinto Charis',
       role: 'hello',
       image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-      gitlink:"https://github.com/Charispinto",
+      gitlink: "https://github.com/Charispinto",
       linked: "https://github.com/Charispinto"
 
     },
     // Add more team members here
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'https://avatars.githubusercontent.com/u/114792058?v=4',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
   ];
 
-  const facultyMembers  = [
+  const facultyMembers = [
     {
-        name: 'Pinto Charis',
-        role: 'hello',
-        image: 'sam.jpg',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Pinto Charis',
+      role: 'hello',
+      image: 'sam.jpg',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     },
     {
       name: 'Pinto Charis',
       role: 'hello',
       image: 'sam.jpg',
-      gitlink:"https://github.com/Charispinto",
+      gitlink: "https://github.com/Charispinto",
       linked: "https://github.com/Charispinto"
 
     },
 
     {
-        name: 'Samwin Pereira',
-        role: 'hello',
-        image: 'sam.jpg',
-        gitlink:"https://github.com/Charispinto",
-        linked: "https://github.com/Charispinto"
+      name: 'Samwin Pereira',
+      role: 'hello',
+      image: 'sam.jpg',
+      gitlink: "https://github.com/Charispinto",
+      linked: "https://github.com/Charispinto"
     }
   ]
 
   return (
-    <><div className="container mx-auto py-8 ">
-
-      <div className='text-center'>
-        <h1 className='text-4xl font-bold mb-9 pb-2 bg-gradient-to-r from-sky-500 to-purple-400 bg-clip-text text-transparent'>Faculty</h1>
+    <div>
+    <div className="container mx-auto py-8 px-20">
+      <div className="text-center">
+      <h1 className="py-10 text-6xl font-bold mb-9 pb-10 bg-gradient-to-r from-sky-500 to-purple-400 bg-clip-text text-transparent">
+          Faculty
+        </h1>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {facultyMembers.map((member, index) => (
-          <div key={index} className="bg-gray-900 hover:bg-gray-800  border-gray-400 border-solid border-1 transition duration-150 ease-out hover:ease-in shadow  p-4 flex flex-col items-center">
-            <img
-              src={member.image}
-              alt={member.name}
-              className="w-48 h-48 object-cover mb-4 rounded-full mt-4 " />
-            <h2 className="text-xl text-white font-bold mb-3">{member.name}</h2>
-            <p className="text-gray-500">{member.role}</p>
-            <div className='flex flex-row justify-center mt-2'>
-              <div className='m-2'><a href={member.gitlink}>{<AiFillGithub color='white' size={30} />}</a></div>
-              <div className='m-2'><a href={member.linked}>{<AiFillLinkedin color='white' size={30} />}</a></div>
-
-            </div>
-          </div>
+          <MemberCard key={index} member={member} />
         ))}
       </div>
 
-
-
-      <div className='text-center mt-3'>
-        <h1 className="text-4xl font-bold mb-8 m-auto bg-gradient-to-r from-sky-500 to-purple-400 bg-clip-text text-transparent">Our Team</h1>
+      <div className="text-center mt-3">
+        <h1 className="py-16 text-6xl font-bold mb-8 m-auto bg-gradient-to-r from-sky-500 to-purple-400 bg-clip-text text-transparent">
+          Our Team
+        </h1>
       </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {teamMembers.map((member, index) => (
-          <div key={index} className="bg-gray-900 hover:bg-gray-800  border-gray-400 border-solid border-1 transition duration-150 ease-out hover:ease-in shadow  p-4 flex flex-col items-center">
-            <img
-              src={member.image}
-              alt={member.name}
-              className="w-48 h-48 object-cover mb-4 rounded-full mt-4" />
-            <h2 className="text-xl text-white font-bold mb-3">{member.name}</h2>
-            <p className="text-gray-500">{member.role}</p>
-            <div className='flex flex-row justify-center mt-2'>
-              <div className='m-2'><a href={member.gitlink}>{<AiFillGithub color='white' size={30} />}</a></div>
-              <div className='m-2'><a href={member.linked}>{<AiFillLinkedin color='white' size={30} />}</a></div>
-
-            </div>
-          </div>
+          <MemberCard key={index} member={member} />
         ))}
-
-
-
       </div>
     </div>
-    <Footer /></>
+    <Footer />
+    </div>
   );
 };
+
 
 export default TeamPage;
 
