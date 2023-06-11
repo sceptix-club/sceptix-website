402c45c40d9394645dc1bd6cdc44f090dbaa54e0
joywin2003 <21a20.joywin@sjec.ac.in>
2023-06-11 11:49:46 +0530

    hover effects

--- src/components/joywin-cards/Card.jsx
+++ src/components/joywin-cards/Card.jsx
@@ -3,41 +3,43 @@ import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import './Card.css';
 
-const Cards = (props) => {
+const EventCards = (props) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  console.log(props)
 
-  const handleCardClick = () => {
-    setIsExpanded(!isExpanded);
+  const handleCardHover = () => {
+    setIsExpanded(true);
+  };
+
+  const handleCardLeave = () => {
+    setIsExpanded(false);
   };
 
   return (
-    <div className={`Card-content ${isExpanded ? 'expanded' : ''}`} onClick={handleCardClick}>
+    <div
+      className={`Card-content ${isExpanded ? 'expanded' : ''}`}
+      onMouseEnter={handleCardHover}
+      onMouseLeave={handleCardLeave}
+    >
       <div className="Card-image">
         <img src={props.image} alt="Test Image" />
       </div>
       <div className="Card-title">
-          <h3>{props.eventName}</h3>
-          <h5>
-            <FontAwesomeIcon icon={faCalendarAlt} /> {props.date}
-          </h5>
+        <h3>{props.eventName}</h3>
+        <h5>
+          <FontAwesomeIcon icon={faCalendarAlt} /> {props.date}
+        </h5>
       </div>
-      {isExpanded ? (
+      {isExpanded && (
         <div className="Card-info">
           <h3>{props.eventName}</h3>
           <h5>
             <FontAwesomeIcon icon={faCalendarAlt} /> {props.date}
           </h5>
-          <p>
-            {props.eventInfo}
-          </p>
+          <p>{props.eventInfo}</p>
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
 
-export default Cards;
-
-
-      
+export default EventCards;
