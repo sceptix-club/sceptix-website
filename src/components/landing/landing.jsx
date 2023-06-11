f9c8bbb0fbd3236f93238c1fe01d978ef839b34d
UthpalSuvarna <uthpal33@gmail.com>
2023-06-11 22:44:09 +0530

    Changed landing page colors

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useSpring, config } from 'react-spring';
-import Sidebar from '../sidenav/sidebar';
+import React, { useEffect, useRef, useState } from "react";
+import { useSpring, config } from "react-spring";
+import Sidebar from "../sidenav/sidebar";
 
 const Landing = () => {
   const leftRef = useRef(null);
@@ -28,34 +28,39 @@ const Landing = () => {
   };
 
   return (
-    <main className="h-screen overflow-hidden relative" onPointerMove={handleOnMove}>  
-
-      <div      
-        className="h-screen w-full overflow-hidden absolute z-20 bg-stone-50 text-stone-950"
+    <main
+      className="h-screen overflow-hidden relative"
+      onPointerMove={handleOnMove}
+    >
+      <div
+        className="h-screen w-full overflow-hidden absolute z-20 bg-cyan-100 text-stone-950"
         id="left-box"
         ref={leftRef}
-        style={{ width: tweenWipe.value.interpolate((val) => `${val}%`) }}      >
-     
+        style={{ width: tweenWipe.value.interpolate((val) => `${val}%`) }}
+      >
         <div className="w-screen h-screen flex flex-col items-center justify-center absolute z-10">
           <h1 className="font-bold text-7xl my-6">
-            The{' '}
-            <span className="code bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-purple-400 ">
+            The{" "}
+            <span className="code bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-600 ">
               sceptix
-            </span>{' '}
+            </span>{" "}
             club
           </h1>
           <h3 className="text-3xl font-mono">Liberate The Mind</h3>
         </div>
-        <Sidebar/>
-      </div>     
+        <Sidebar />
+      </div>
 
-      <div className="h-screen w-full overflow-hidden absolute z-10 bg-stone-950 text-stone-50" id="right-box">
+      <div
+        className="h-screen w-full overflow-hidden absolute z-10 bg-[#00070e] text-stone-50"
+        id="right-box"
+      >
         <div className="w-full h-screen flex flex-col items-center justify-center absolute z-10">
           <h1 className="font-bold text-7xl my-6">
-            The{' '}
-            <span className="code bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-sky-400">
+            The{" "}
+            <span className="code bg-clip-text text-transparent bg-gradient-to-r from-[#2afadf] to-sky-600">
               freedom
-            </span>{' '}
+            </span>{" "}
             club
           </h1>
           <h3 className="text-3xl font-mono">Unchain Your Will</h3>
