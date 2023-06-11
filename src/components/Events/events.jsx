1c468f88130d4eb1ad9c7697f4c092d7d2105631
UthpalSuvarna <uthpal33@gmail.com>
2023-06-11 21:50:22 +0530

    Added event image and date

--- src/components/Events/events.jsx
+++ src/components/Events/events.jsx
@@ -1,58 +1,57 @@
-import React from 'react';
-import {AiFillGithub} from 'react-icons/ai'
-import { AiFillLinkedin } from 'react-icons/ai'; 
-import Footer from '../footer/footer.jsx';
-import Cards from '../joywin-cards/Card.jsx';
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import Footer from "../footer/footer.jsx";
+import Cards from "../joywin-cards/Card.jsx";
 
-
-
-
-  const EventPage = () => {
-   const eventPast = [
+const EventPage = () => {
+  const eventPast = [
     {
-    eventName: "hacktofuture",    
-    date: "July 2022",
-    image: 'hack.jpg',
-    eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
-  
-    }, 
-
+      eventName: "JS Jumpstart",
+      date: "12/6/2023",
+      image: "Final-01.jpeg",
+      eventInfo:
+        "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot",
+    },
     {
-        eventName: "hacktofuture",    
-        date: "July 2022",
-        image: 'hack.jpg',
-        eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
-      
-    }, 
+      eventName: "Hack To Future",
+      date: "25/4/2023",
+      image: "HackToFuture - 26 April 2023.jpg",
+      eventInfo:
+        "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot",
+    },
 
     {
-        eventName: "hacktofuture",    
-        date: "July 2022",
-        image: 'hack.jpg',
-        eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
-      
-    }, 
-
+      eventName: "A Guide To ARG",
+      date: "17/3/2023",
+      image: "A Guide To ARG- 17 March 2023-01.jpeg",
+      eventInfo:
+        "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot",
+    },
+    {
+      eventName: "SJEC Premiere Hackathon",
+      date: "25/1/2023",
+      image: "SJEC Premiere Hackathon - 25 Jan 2023.jpg",
+      eventInfo:
+        "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot",
+    },
+    {
+      eventName: "SJEC 30 Days Of Code",
+      date: "19/12/2022",
+      image: "sjec-30-days-of-code-01.jpeg",
+      eventInfo:
+        "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot",
+    },
 
     {
-        eventName: "hacktofuture",    
-        date: "July 2022",
-        image: 'hack.jpg',
-        eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
-      
-    }, 
-
-    {
-        eventName: "hacktofuture",    
-        date: "July 2022",
-        image: 'hack.jpg',
-        eventInfo: "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot"
-      
-    }, 
-
-
-]
-return (
+      eventName: "Inauguration",
+      date: "16/6/2022",
+      image: "Inauguration - 16 June 2022-01.jpeg",
+      eventInfo:
+        "some random event took place this summer i dont know what to put here how did feel dont smile too much you idiot",
+    },
+  ];
+  return (
     <>
       <div className="container mx-auto py-8 bg-black">
         <div className="text-center mt-3">
@@ -72,4 +71,6 @@ return (
 };
 
 export default EventPage;
-{/*w-full  h-41*/}
+{
+  /*w-full  h-41*/
+}
