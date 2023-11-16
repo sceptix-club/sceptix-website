883fd2bd0f02404fc889139e51da6c4c89e261df
vyshnav <vyshnavravi599@gmail.com>
2023-11-16 08:29:23 +0530

    added a scroll function

--- src/components/sidenav/mobileviewSidebar.jsx
+++ src/components/sidenav/mobileviewSidebar.jsx
@@ -5,6 +5,13 @@ import { RiTeamLine } from 'react-icons/ri';
 import {AiOutlineHome} from 'react-icons/ai'
 
 function MobileviewSidebar() {
+    let HandleClick = ()=>{ 
+        window.scrollTo({
+          top:0,
+          behavior:'smooth'
+        })
+        
+      }
   return (
     
         <>
@@ -12,7 +19,7 @@ function MobileviewSidebar() {
             
     <div style={{backgroundColor:'#D3D3D3'}} className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
         <div className="grid h-full max-w-lg grid-cols-3 mx-auto">
-            <button type="button" className="inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+            <button onClick={HandleClick} type="button" className="inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
             <Link to='/members'>
                 <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   
@@ -21,7 +28,7 @@ function MobileviewSidebar() {
                     </Link>
                 <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Members</span>
             </button>
-            <button type="button" className="inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+            <button onClick={HandleClick} type="button" className="inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                 <Link to="/events">
 
                 <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 19">
@@ -30,7 +37,7 @@ function MobileviewSidebar() {
                 </Link>
                 <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Events</span>
             </button>
-            <button type="button" className="inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+            <button onClick={HandleClick} type="button" className="inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                 <Link to='/'>
 
                 <svg className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
