a4518fea1b18ea6ddba13a15552a4a06ec8af38b
vyshnav <vyshnavravi599@gmail.com>
2023-11-29 18:59:25 +0530

    updated

---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <>
+    <div className='flex justify-center h-screen items-center'>
+        <h1 className='text-white text-5xl font-extrabold'>404 Page Not Found</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default PageNotFound
